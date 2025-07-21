f7de24604cbae221464e37a8baa295b05d91eff6
ten00m <ilya.safronov.1989@gmail.com>
2025-07-21 21:21:00 +0300

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-
+    import Header from './components/Header.vue'
 </script>
 
 <template>
-  <main>
-    
-  </main>
+    <main>
+        <div class="accounts">
+            <Header></Header>
+        </div>
+    </main>
 </template>
 
 <style scoped>
-
-
+    .accounts{
+        width: 500px;
+        height: 200px;
+        border: 2px solid gray;
+        padding: 10px;
+        border-radius: 10px;
+    }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import {NButton} from 'naive-ui'
+    import {NPageHeader} from 'naive-ui'
+</script>
+<template>
+    <div class="accounts-header">
+        <h1>Учетные записи</h1>
+        <NButton>+</NButton>
+    </div>
+</template>
+<style scoped>
+    h1{
+        font-size: 20px;
+        width: 150px;
+    }
+    .accounts-header{
+        display: flex;
+        flex-direction: row;
+        width: 200px;
+        justify-content: space-between;
+        align-items: end;
+    }
+
+</style>
